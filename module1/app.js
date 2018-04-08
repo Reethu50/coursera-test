c7b0ac998f42afaddfec59631cc4a45c5ccd0a13
reethu <reethu@something.com>
2018-04-08 20:28:34 +0530

    modified

--- module1/app.js
+++ module1/app.js
@@ -8,22 +8,22 @@
     $scope.foodItems = ""
     $scope.message = "";
     $scope.checkLunchItems = function() {
-      $scope.foodItems = $scope.foodItems.trim();
+  //    $scope.foodItems = $scope.foodItems.trim();
 
-      if ($scope.foodItems === "") {
+      if ($scope.foodItems.trim() === "") {
         $scope.message = "Please enter data first.";
-        $scope.noticeType = "danger";
+        //$scope.noticeType = "danger";
       }
       else {
-        var $foodItemsArray = $scope.foodItems.split(",");
-        var $length = $foodItemsArray.length;
-        if ( ($length <= 3) || ($length == 4 && $foodItemsArray[3].trim() === "") ) {
+        var foodItemsArray = $scope.foodItems.split(",");
+        var length = $foodItemsArray.length;
+        if ( (length <= 3) || (length == 4 && foodItemsArray[3].trim() === "") ) {
           $scope.message = "Enjoy!";
-          $scope.noticeType = "success";
+        //  $scope.noticeType = "success";
         }
         else {
           $scope.message = "Too much!";
-          $scope.noticeType = "warning";
+        //  $scope.noticeType = "warning";
         }
       }
     };
